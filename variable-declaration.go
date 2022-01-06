65e66f42e95ae42a07a358dba2198b6c48f7d2ad
sambandham143 <80306541+sambandham143@users.noreply.github.com>
2022-01-06 15:07:25 +0530

    Create variable-declaration.go

---
+++ variable-declaration.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	card := "Ace of Spades"   // assigning a new variable use colon
+	card = "Five of Diamonds" // for assigning values dont use colon
+	fmt.Println(card)
+}
