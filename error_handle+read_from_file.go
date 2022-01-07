14cd43b45528b3575248c4e3702a05d87c60d05b
sambandham143 <80306541+sambandham143@users.noreply.github.com>
2022-01-07 08:45:27 +0530

    Create error_handle+read_from_file.go

---
+++ error_handle+read_from_file.go
@@ -0,0 +1,84 @@
+################## MAIN.GO ###################
+
+
+
+package main
+
+func main() {
+	cards := newDeckFromFile("my")
+
+	cards.print()
+
+}
+
+
+
+
+
+#################.  DECK.GO ##################
+
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+type deck []string
+
+////// This is for creating a New Deck
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suite := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suite)
+		}
+	}
+	return cards
+}
+
+//////// This is for Printing
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+///////// This is a deal function
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+/////// THis is string conversion function
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+//// This is saving a file to a machine function
+
+func (d deck) saveToFile(fileName string) error {
+	return ioutil.WriteFile(fileName, []byte(d.toString()), 0666)
+}
+
+///// Reading a file from a machine function
+
+func newDeckFromFile(fileName string) deck {
+	bs, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
