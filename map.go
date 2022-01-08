644496500251f10bc44446d0aba9862479d4049a
sambandham143 <80306541+sambandham143@users.noreply.github.com>
2022-01-08 09:25:37 +0530

    Create map.go

---
+++ map.go
@@ -0,0 +1,14 @@
+########## MAIN.GO ###########
+
+
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":   "#FF0000",
+		"green": "#00FF00",
+	}
+	fmt.Println(colors)
+}
